80b5a9c54213b3df316ddb07bbbc8b89a317bf2c
swyoon <sangwoong24yoon@gmail.com>
2022-11-05 07:01:27 +0000

    update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,5 @@
 # python train.py --outdir=./training-runs/cars --cfg=fastgan_lite --data=./data/cars_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=10000
-python train.py --outdir=./training-runs/cars --cfg=fastgan_lite --data=./data/cars_test_prep64 --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000
+python train.py --outdir=./training-runs/cars --cfg=stylegan2 --data=./data/cars_test_prep64 --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000
 
 
 
