a0621d788a3dbe70bd0de2ae21f7d2bf9c9ef254
swyoon <sangwoong24yoon@gmail.com>
2022-11-05 15:08:13 +0000

    update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,5 +1,5 @@
 # python train.py --outdir=./training-runs/cars --cfg=fastgan_lite --data=./data/cars_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=10000
-python train.py --outdir=./training-runs/cars --cfg=stylegan2 --data=./data/cars_test_prep64 --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000
+# python train.py --outdir=./training-runs/cars-z16 --cfg=stylegan2 --data=./data/cars_test_prep64 --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 16
 
 
 
@@ -7,3 +7,4 @@ python train.py --outdir=./training-runs/cars --cfg=stylegan2 --data=./data/cars
 # python train.py --outdir=./training-runs/fgvc --cfg=fastgan_lite --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=5000
 # python train.py --outdir=./training-runs/fgvc-z16 --cfg=fastgan_lite --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=5000 --zdim 16
 # python train.py --outdir=./training-runs/fgvc-z16 --cfg=stylegan2 --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=5000 --zdim 16
+python train.py --outdir=./training-runs/fgvc-z64 --cfg=stylegan2 --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 64
