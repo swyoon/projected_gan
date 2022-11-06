4d6b26e1fc61ca4b552c2de276fd48e8a08074f8
Sangwoong Yoon <sangwoong24yoon@gmail.com>
2022-11-06 02:32:20 +0000

    update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -7,4 +7,9 @@
 # python train.py --outdir=./training-runs/fgvc --cfg=fastgan_lite --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=5000
 # python train.py --outdir=./training-runs/fgvc-z16 --cfg=fastgan_lite --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=5000 --zdim 16
 # python train.py --outdir=./training-runs/fgvc-z16 --cfg=stylegan2 --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=8 --kimg=5000 --zdim 16
-python train.py --outdir=./training-runs/fgvc-z64 --cfg=stylegan2 --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 64
+# python train.py --outdir=./training-runs/fgvc-z64 --cfg=stylegan2 --data=./data/FGVC_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 64
+# python train.py --outdir=./training-runs/fgvc-z16 --cfg=stylegan2 --data=./data/FGVC_test_prep64 --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 16
+
+
+### Flowers without insects
+python train.py --outdir=./training-runs/flowers-wo-insects --cfg=stylegan2 --data=./data/flowers_wo_insects_test_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 16
