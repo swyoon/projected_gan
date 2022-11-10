d58911afc9f56f52599b2d9cd525b8abd91db1af
Sangwoong Yoon <sangwoong24yoon@gmail.com>
2022-11-10 07:05:16 +0000

    update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -17,10 +17,14 @@
 
 # CelebA 32
 # CUDA_VISIBLE_DEVICES=0,1 python train.py --outdir=./training-runs/celeba32 --cfg=stylegan2 --data=./data/celeba32_prep --gpus=2 --batch=64 --mirror=1 --snap=50 --batch-gpu=32 --kimg=5000 --zdim 16
+# CUDA_VISIBLE_DEVICES=0,1 python train.py --outdir=./training-runs/celeba32 --cfg=stylegan2 --data=./data/celeba32_prep --gpus=2 --batch=64 --mirror=1 --snap=50 --batch-gpu=32 --kimg=10000 --zdim 16 --resume=training-runs/celeba32/00001-stylegan2-celeba32_prep-gpus2-batch64/network-snapshot.pkl
+# CUDA_VISIBLE_DEVICES=2,3 python train.py --outdir=./training-runs/celeba32-z32 --cfg=stylegan2 --data=./data/celeba32_prep --gpus=2 --batch=64 --mirror=1 --snap=50 --batch-gpu=32 --kimg=5000 --zdim 32 
+# CUDA_VISIBLE_DEVICES=0,1 python train.py --outdir=./training-runs/celeba32-z128 --cfg=stylegan2 --data=./data/celeba32_prep --gpus=2 --batch=64 --mirror=1 --snap=50 --batch-gpu=32 --kimg=5000 --zdim 128
+CUDA_VISIBLE_DEVICES=2,3 python train.py --outdir=./training-runs/celeba32-z64 --cfg=stylegan2 --data=./data/celeba32_prep --gpus=1 --batch=64 --mirror=1 --snap=50 --batch-gpu=64 --kimg=5000 --zdim 64
 
 # SVHN
 # CUDA_VISIBLE_DEVICES=2,3 python train.py --outdir=./training-runs/svhn_test_nohflip --cfg=stylegan2 --data=./data/svhn_test_prep --gpus=2 --batch=64 --mirror=0 --snap=50 --batch-gpu=32 --kimg=5000 --zdim 16
 
 
 # EuroSAT
-python train.py --outdir=./training-runs/eurosat --cfg=stylegan2 --data=./data/eurosat_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 16
+# python train.py --outdir=./training-runs/eurosat --cfg=stylegan2 --data=./data/eurosat_prep --gpus=4 --batch=64 --mirror=1 --snap=50 --batch-gpu=16 --kimg=5000 --zdim 16
